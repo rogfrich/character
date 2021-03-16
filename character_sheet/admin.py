988a9fd574a6c5f2ce5ad2867103d687de55b2d5
Rich <richard.a.cairns@gmail.com>
2021-03-16 16:41:09 +0000

    fix bug in admin site registration for models

--- character_sheet/admin.py
+++ character_sheet/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Character, CharacterClass, Race
 # Register your models here.
-admin.register(Character)
-admin.register(CharacterClass)
-admin.register(Race)
+admin.site.register(Character)
+admin.site.register(Race)
+admin.site.register(CharacterClass)
