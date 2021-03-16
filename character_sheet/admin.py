2fe931da89354a7c6562807200e1dd23638d59a4
Rich <richard.a.cairns@gmail.com>
2021-03-16 16:35:20 +0000

    created basic models and first migration file

--- character_sheet/admin.py
+++ character_sheet/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Character, CharacterClass, Race
 # Register your models here.
+admin.register(Character)
+admin.register(CharacterClass)
+admin.register(Race)
--- character_sheet/models.py
+++ character_sheet/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class CharacterClass(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    level = models.IntegerField(default=1)
+    race = models.ForeignKey(Race, on_delete=models.PROTECT)
+    character_class = models.ForeignKey(CharacterClass, on_delete=models.PROTECT)
