68d31f2ee99aea438efca08ecf9416046a8b4ee8
Rich <richard.a.cairns@gmail.com>
2021-03-16 17:10:26 +0000

    add __str__() methods to all models

--- character_sheet/models.py
+++ character_sheet/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class Race(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
 
 class CharacterClass(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -13,9 +15,15 @@ class CharacterClass(models.Model):
         verbose_name = "Class"
         verbose_name_plural = "Classes"
 
+    def __str__(self):
+        return self.name
+
 
 class Character(models.Model):
     name = models.CharField(max_length=50, unique=True)
     level = models.IntegerField(default=1)
     race = models.ForeignKey(Race, on_delete=models.PROTECT)
     character_class = models.ForeignKey(CharacterClass, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
