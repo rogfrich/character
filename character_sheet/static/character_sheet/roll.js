0580518518e96430c29d57f879d7b1ec24155260
Rich <richard.a.cairns@gmail.com>
2021-03-22 14:06:02 +0000

    display nicely formatted modifier values in index and roll alerts

--- character_sheet/static/character_sheet/roll.js
+++ character_sheet/static/character_sheet/roll.js
@@ -1,5 +1,9 @@
         function roll(sides, mod) {
+            let sign = "";
+            if (mod >= 0) {
+                sign = "+"
+            }
             let result = Math.floor((Math.random() * sides) + 1);
-            result_output = `You roll ${result} with a modifier of ${mod} for a total of ${result + mod}`
+            result_output = `You roll ${result} with a modifier of ${sign}${mod} for a total of ${result + mod}`;
             alert(result_output);
         }
