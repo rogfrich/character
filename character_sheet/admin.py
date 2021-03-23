2d783def6b94772a12190196b86027e660959c08
Rich <richard.a.cairns@gmail.com>
2021-03-23 12:02:07 +0000

    closes #6 - build weapons model

--- character_sheet/admin.py
+++ character_sheet/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Character, CharacterClass, Race
+from .models import \
+    Character, \
+    CharacterClass, \
+    Race, \
+    Weapon, \
+    WeaponCategory, \
+    Damage, \
+    DamageType
+
 # Register your models here.
 admin.site.register(Character)
 admin.site.register(Race)
 admin.site.register(CharacterClass)
+admin.site.register(Weapon)
+admin.site.register(WeaponCategory)
+admin.site.register(DamageType)
+admin.site.register(Damage)
